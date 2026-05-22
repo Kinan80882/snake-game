43c1f377df6614f6d43753e3394a65764394d486
Kinan80882 <tempmail8088@protonmail.com>
2026-05-22 08:52:12 +0200

    Fixed bug of colliding with yourself again

--- snake.py
+++ snake.py
@@ -85,19 +85,19 @@ def gameLoop():
                 game_over = True
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_a and current_direction != "right":
+                if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and current_direction != "right":
                     x1_change = -snake_block
                     y1_change = 0
                     current_direction = "left"
-                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d and current_direction != "left":
+                elif (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and current_direction != "left":
                     x1_change = snake_block
                     y1_change = 0
                     current_direction = "right"
-                elif event.key == pygame.K_UP or event.key == pygame.K_w and current_direction != "down":
+                elif (event.key == pygame.K_UP or event.key == pygame.K_w) and current_direction != "down":
                     y1_change = -snake_block
                     x1_change = 0
                     current_direction = "up"
-                elif event.key == pygame.K_DOWN or event.key == pygame.K_s and current_direction != "up":
+                elif (event.key == pygame.K_DOWN or event.key == pygame.K_s) and current_direction != "up":
                     y1_change = snake_block
                     x1_change = 0
                     current_direction = "down"
